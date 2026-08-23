851a35206b9a81b7ba2a2bf13ac59e5c99520fcc
Manthan Garg <manthangarg2024cse@gmail.com>
2026-08-23 18:24:05 +0530

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return solve(m - 1, n - 1, dp);
+    }
+    public int solve(int i, int j, int[][] dp) {
+        if (i == 0 && j == 0)
+            return 1;
+        if (i < 0 || j < 0)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int take = solve(i - 1, j, dp);
+        int notTake = solve(i, j - 1, dp);
+        return dp[i][j] = take + notTake;
+    }
+}
